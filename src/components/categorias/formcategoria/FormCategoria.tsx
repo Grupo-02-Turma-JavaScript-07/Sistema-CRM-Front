51a27a1cf9661de416b53a32b44ddc6e13539b15
Adrielli-Bertoldo <adrielli.bertoldo@gmail.com>
2025-08-09 12:10:33 -0300

    atualização cod categoria

--- src/components/categorias/formcategoria/FormCategoria.tsx
+++ src/components/categorias/formcategoria/FormCategoria.tsx
@@ -1,116 +1,107 @@
-// import { useState, useEffect, type ChangeEvent } from "react";
-// import { useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect, type ChangeEvent, type FormEvent } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
 
-// import { buscar, atualizar, cadastrar } from "../../../services/Service";
-// import type Categoria from "../../../models/Categoria";
+import { buscar, atualizar, cadastrar } from "../../../services/Service";
+import type Categoria from "../../../models/Categoria";
 
-// interface FormCategoriaProps {
-//     categoria: Categoria;
-// }
+function FormCategoria() {
+    const navigate = useNavigate();
 
-// function FormCategoria({ categoria }: FormCategoriaProps) {
+    const [categoriaState, setCategoriaState] = useState<Categoria>({} as Categoria);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
-//     const navigate = useNavigate();
+    const { id } = useParams<{ id: string }>();
 
-//     const [categoria, setCategoria] = useState<Categoria>({} as Categoria)
-//     const [isLoading, setIsLoading] = useState<boolean>(false)
+    async function buscarPorId(id: string) {
+        try {
+            await buscar(`/categorias/${id}`, setCategoriaState);
+        } catch (error: any) {
+            console.error(error);
+        }
+    }
 
-//     const { id } = useParams<{ id: string }>();
+    useEffect(() => {
+        if (id !== undefined) {
+            buscarPorId(id);
+        }
+    }, [id]);
 
-//     async function buscarPorId(id: string) {
-//         try {
-//             await buscar(`/categorias/${id}`, setCategoria)
-//         } catch (error: any) { /* empty */ }
-//     }
+    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+        setCategoriaState({
+            ...categoriaState,
+            [e.target.name]: e.target.value,
+        });
+    }
 
-//     useEffect(() => {
-//         if (id !== undefined) {
-//             buscarPorId(id)
-//         }
-//     }, [id])
+    function retornar() {
+        navigate("/categorias");
+    }
 
-//     function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-//         setCategoria({
-//             ...categoria,
-//             [e.target.name]: e.target.value
-//         })
-//     }
+    async function gerarNovaCategoria(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        setIsLoading(true);
 
-//     function retornar() {
-//         navigate("/categorias")
-//     }
+        if (id !== undefined) {
+            try {
+                await atualizar(`/categorias`, categoriaState, setCategoriaState);
+                alert("A Categoria foi atualizada com sucesso!");
+            } catch (error: any) {
+                console.error(error);
+                alert("Erro ao atualizar categoria: " + error.message);
+            }
+        } else {
+            try {
+                await cadastrar(`/categorias`, categoriaState, setCategoriaState);
+                alert("A Categoria foi cadastrada com sucesso!");
+            } catch (error: any) {
+                console.error(error);
+                alert("Erro ao cadastrar categoria: " + error.message);
+            }
+        }
 
-//     async function gerarNovaCategoria(e: ChangeEvent<HTMLFormElement>) {
-//         e.preventDefault()
-//         setIsLoading(true)
+        setIsLoading(false);
+        retornar();
+    }
 
-//         if (id !== undefined) {
-//             try {
-//                 await atualizar(`/categorias`, categoria, setCategoria)
-//                 alert('A Categoria foi atualizada com sucesso!')
-//             } catch (error: any) {
-//                 console.error(error);
-//                 alert("Erro ao buscar categoria: " + error.message);
-//             }
-            
-//         } else {
-//             try {
-//                 await cadastrar(`/categorias`, categoria, setCategoria)
-//                 alert('A Categoria foi cadastrada com sucesso!')
-//             } catch (error: any) {
-//                 console.error(error);
-//                 alert("Erro ao buscar categoria: " + error.message);
-//             }
+    return (
+        <div className="container flex flex-col items-center justify-center mx-auto">
+            <h1 className="text-4xl text-center my-8">
+                {id === undefined ? "Cadastrar Categoria" : "Editar Categoria"}
+            </h1>
 
-//             }
-//         }
+            <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovaCategoria}>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="descricao">Descrição da Categoria</label>
+                    <input
+                        type="text"
+                        placeholder="Descreva aqui sua categoria"
+                        name="descricao"
+                        className="border-2 border-slate-700 rounded p-2"
+                        value={categoriaState.descricao || ""}
+                        onChange={atualizarEstado}
+                    />
+                </div>
+                <button
+                    className="rounded text-slate-100 bg-indigo-400 
+                            hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
+                    type="submit"
+                >
+                    {isLoading ? (
+                        <RotatingLines
+                            strokeColor="white"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="24"
+                            visible={true}
+                        />
+                    ) : (
+                        <span>{id === undefined ? "Cadastrar" : "Atualizar"}</span>
+                    )}
+                </button>
+            </form>
+        </div>
+    );
+}
 
-//         setIsLoading(false)
-//         retornar()
-//     }
-
-//     return (
-//                                 // HTML REAPROVEITÁVEL
-
-
-
-//         <div className="container flex flex-col items-center justify-center mx-auto">
-//             <h1 className="text-4xl text-center my-8">
-//                 {id === undefined ? 'Cadastrar Categoria' : 'Editar Categoria'}
-//             </h1>
-
-//             <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovaCategoria}>
-//                 <div className="flex flex-col gap-2">
-//                     <label htmlFor="descricao">Descrição da Categoria</label>
-//                     <input
-//                         type="text"
-//                         placeholder="Descreva aqui sua categoria"
-//                         name='descricao'
-//                         className="border-2 border-slate-700 rounded p-2"
-//                         value={categoria.descricao}
-//                         onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-//                     />
-//                 </div>
-//                 <button
-//                     className="rounded text-slate-100 bg-indigo-400 
-//                             hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
-//                     type="submit">
-//                     {isLoading ?
-//                         <RotatingLines
-//                             strokeColor="white"
-//                             strokeWidth="5"
-//                             animationDuration="0.75"
-//                             width="24"
-//                             visible={true}
-//                         /> :
-//                         <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
-
-//                     }
-//                 </button>
-//             </form>
-//         </div>
-//     );
-
-// }
-
-// export default FormCategoria;
+export default FormCategoria;
