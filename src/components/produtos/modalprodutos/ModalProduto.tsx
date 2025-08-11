f39ea55d19e2f23c447bfb4714659dd93f2f092f
Natalia Taira <nataliagtaira@gmail.com>
2025-08-11 10:40:58 -0300

    Ajusta a posição do botão Cadastrar Produto

--- src/components/produtos/modalprodutos/ModalProduto.tsx
+++ src/components/produtos/modalprodutos/ModalProduto.tsx
@@ -10,10 +10,11 @@ function ModalProduto() {
             <Popup
                 trigger={
                     <button 
-                    className="px-6 py-2 rounded-md gap-2 text-white font-semibold bg-gradient-to-r from-indigo-500 to-purple-500 hover:opacity-90 flex justify-center items-center"
-                    >    
-                    <FiPlus />                    
-                    Cadastrar Produto
+                    className="px-6 py-2 mx-9 rounded-md gap-2 text-white font-semibold bg-gradient-to-r
+                    from-indigo-500 to-purple-500 hover:opacity-90 flex justify-center items-center"
+                    >
+                        <FiPlus />                
+                        Cadastrar Produto
                     </button>
                 }
                 modal
