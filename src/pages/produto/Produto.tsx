cab1ba0b6f19fc35a1b2aedd8af54a31ab45e6c9
devvMiguel <miguelgalvao_galvao@hotmail.com>
2025-08-11 10:12:22 -0300

    feat: implementei funcionalidade especial de busca por produtos entre dois preços

--- src/pages/produto/Produto.tsx
+++ src/pages/produto/Produto.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import NavbarPerfil from '../../components/navbarperfil/NavbarPerfil'
+import ProcurarEntrePrecos from '../../components/produtos/funcionalidade/Funcionalidade'
 import ListaProdutos from '../../components/produtos/listarprodutos/ListaProdutos'
 
 function Produto() {
+  const [precoMin, setPrecoMin] = useState<number | undefined>(undefined);
+  const [precoMax, setPrecoMax] = useState<number | undefined>(undefined);
+  
   return (
     <div>
       <NavbarPerfil/>
-      <ListaProdutos />
+       <ProcurarEntrePrecos
+          onFiltrar={(min, max) => {
+            setPrecoMin(min || undefined);
+            setPrecoMax(max || undefined);
+          }}
+        />
+      <ListaProdutos precoMin={precoMin} precoMax={precoMax} />
       {/* <DeletarProduto /> */}
     </div>
   )
